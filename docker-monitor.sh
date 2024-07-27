981413d2f404da49bdbb43a0f8b00d2e31a2bea3
saarthak <markandeysaarthak347@gmail.com>
2024-07-27 13:46:34 +0530

    Readme instructions + docker-monitor.sh improvements
    Signed-off-by: saarthak <markandeysaarthak347@gmail.com>

--- docker-monitor.sh
+++ docker-monitor.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Start the containers in detached mode
-sudo docker-compose -f /home/saarthak/Desktop/scm-probe/scm-prom/monitor.docker-compose.yaml up -d
+# Check if the path to the docker-compose file is provided
+if [ -z "$1" ]; then
+  echo "Error: No docker-compose file path provided."
+  echo "Usage: $0 /path/to/docker-compose.yaml"
+  exit 1
+fi
+
+DOCKER_COMPOSE_FILE=$1
+
+# Check if the provided file exists
+if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
+  echo "Error: File '$DOCKER_COMPOSE_FILE' does not exist."
+  exit 1
+fi
+
 
 # Get the container IDs
-CONTAINER_IDS=$(sudo docker-compose -f /home/saarthak/Desktop/scm-probe/scm-prom/monitor.docker-compose.yaml ps -q)
+CONTAINER_IDS=$(sudo docker-compose -f "$DOCKER_COMPOSE_FILE" ps -q)
 
 # Print the container IDs
 echo "Started containers with IDs:"
@@ -22,7 +35,3 @@ FIRST_PID=$(echo "$PIDS" | head -n 1)
 echo ${FIRST_PID}
 
 sudo ./main -id ${FIRST_PID}
-
-
-
-
